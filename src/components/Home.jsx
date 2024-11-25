5dffc966b090c9dfdef643b20f93835a73b3d806
Abhijeet Kumar <abkumar4@q3infotech.net>
2024-11-25 12:52:31 +0530

    pwa app

---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='row'>
+    <div className='col-md-4 col-xs-12'></div>
+    <div className='col-md-4 col-xs-12 text-center'>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSohLdS2zHtvmTTASOrUKOsGrQHUga-l1JxRQ&s" alt="movie banner" className='img img-thumbnail'/>
+        <h3>Movie Browser App</h3>
+        <p>Created by Abhijeet</p>
+    </div>
+    <div className='col-md-4 col-xs-12'></div>
+    </div>
+  )
+}
+
+export default Home
